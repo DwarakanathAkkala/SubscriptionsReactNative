e36999047b94aee9f8a417f6eb558bcbc9318da2
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2026-07-22 20:28:43 +0530

    SafeAreaView for Screens

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+// Stytled Wrapped enable ClassName Support for NativeWind on Third-party Component
+const SafeAreaView = styled(RNSafeAreaView);
 
 const Insights = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Insights</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+// Stytled Wrapped enable ClassName Support for NativeWind on Third-party Component
+const SafeAreaView = styled(RNSafeAreaView);
 
 const Subscriptions = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Subscriptions</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
