e60cb427840ab9838ba45e11ca6f6f98c689234f
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2026-07-22 19:49:43 +0530

    Bottom Tabs Navigation with Icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import "@/global.css";
 import { Stack } from "expo-router";
 
+export const unstable_settings = {
+  initialRouteName: "(tabs)",
+};
+
 export default function RootLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
